ba97a29dd9c73d39a91f08f6cd60eacfdac5aee8
545n shahin <mohammadshahinkhan1992@gmail.com>
2022-09-03 10:19:33 +0600

    edite

--- src/Assignment/Banner.js
+++ src/Assignment/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
         <div>
             <div className="carousel w-full">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img src={img1} alt="" /> /
+                    <img src={img1} alt="" /> 
                         <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                             <a href="#slide4" className="btn btn-circle">❮</a>
                             <a href="#slide2" className="btn btn-circle">❯</a>
